1c5d194cf9beb830273d68d4d53310e80a768e84
Florian Viseux <frinola.games@gmail.com>
2024-06-07 16:06:36 +0200

    fix bug vs

---
+++ LabResearchTester/PlayerTester.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace LabResearchTester
+{
+    public class PlayerInfos
+    {
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("age")]
+        public int Age { get; set; }
+    }
+
+    public class PlayerTester
+    {
+        string path = "";
+
+        public PlayerTester(string path)
+        {
+            this.path = path;
+        }
+
+        public PlayerInfos GetPlayerInfos()
+        {
+            string jSonString = File.ReadAllText(path);
+
+            using (var reader = new StringReader(jSonString))
+            {
+                string content = reader.ReadToEnd();
+                Console.WriteLine(content);
+            }
+
+            return null;
+        }
+    }
+}
